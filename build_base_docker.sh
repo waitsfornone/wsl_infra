4dadd93cd5d2f3ae2d97e8696f355073d5324388
Tim Enders <tim.enders@dycominc.com>
2020-02-14 15:28:11 -0600

    got everything up and running with failover

--- build_base_docker.sh
+++ build_base_docker.sh
@@ -5,4 +5,4 @@
 # I may need to login to Docker Hub
 docker build -f mongo/Dockerfile -t waitsfornone/my_repo:mongo . && docker push waitsfornone/my_repo:mongo
 docker build -f rabbitmq/Dockerfile -t waitsfornone/my_repo:rabbitmq . && docker push waitsfornone/my_repo:rabbitmq
-# docker build -f JenkinsDocker -t waitsfornone/my_repo:jenkins && docker push waitsfornone/my_repo:jenkins
+# docker build -f jenkins/Dockerfile -t waitsfornone/my_repo:jenkins . && docker push waitsfornone/my_repo:jenkins
--- docker_start_containers.sh
+++ docker_start_containers.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker run -d -p 27017:27017 --name MongoDB -v /home/tenders/mongodb:/var/lib/mongodb waitsfornone/my_repo:mongo
-docker run -d -p 25672:25672 --name PeterRabbit -p 4369:4369 -p 5671:5671 -p 5672:5672 -p 15672:15672 waitsfornone/my_repo:rabbitmq
+docker run -d --name MongoDB -p 27017:27017 -v /home/tenders/mongodb:/var/lib/mongodb waitsfornone/my_repo:mongo
+docker run -d --name PeterRabbit -h cottontail -p 25672:25672 -p 4369:4369 -p 5671:5671 -p 5672:5672 -p 15672:15672 -v /home/tenders/rabbitmq:/var/lib/rabbitmq waitsfornone/my_repo:rabbitmq
+docker run -d --name Jeeves -p 50000:50000 -p 8080:8080 waitsfornone/my_repo:jenkins
